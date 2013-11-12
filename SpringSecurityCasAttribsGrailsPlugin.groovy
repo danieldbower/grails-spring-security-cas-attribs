ab59fcd966acd08fe3e79b908c4360ef189ed738
Burt Beckwith <burt@burtbeckwith.com>
2013-11-12 00:20:53 -0500

    cleanup

--- SpringSecurityCasAttribsGrailsPlugin.groovy
+++ SpringSecurityCasAttribsGrailsPlugin.groovy
@@ -5,34 +5,22 @@ import org.codehaus.groovy.grails.plugins.springsecurity.cas.DomainUserMapperSer
 import org.jasig.cas.client.validation.Saml11TicketValidator
 
 class SpringSecurityCasAttribsGrailsPlugin {
-    // the plugin version
-    String version = '1.1.1'
+	String version = '1.1.1'
 	String grailsVersion = '1.2.3 > *'
-    // the other plugins this plugin depends on
-    def dependsOn = ['springSecurityCas': '1.0 > *']
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+	def dependsOn = ['springSecurityCas': '1.0 > *']
 
-    def author = "Daniel Bower"
-    def authorEmail = "daniel.d.bower@gmail.com"
-    def title = "Spring Security Cas with Cas Attribs"
-    def description = '''\\
-Allows Grails to obtain authorities directly from CAS.  
-Also allows grails to create the user's profile from attributes in CAS in the app if they do not already exist.  
+	def author = "Daniel Bower"
+	def authorEmail = "daniel.d.bower@gmail.com"
+	def title = "Spring Security Cas with Cas Attribs"
+	def description = '''\
+Allows Grails to obtain authorities directly from CAS.
+Also allows Grails to create the user's profile from attributes in CAS in the app if they do not already exist.
 At this time, ignores local role table.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/spring-security-cas-attribs"
+	def documentation = "http://grails.org/plugin/spring-security-cas-attribs"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
+	def doWithSpring = {
 
 		def conf = SpringSecurityUtils.securityConfig
 
@@ -40,9 +28,9 @@ At this time, ignores local role table.
 			casTicketValidator(Saml11TicketValidator, conf.cas.serverUrlPrefix) {
 				renew = conf.cas.sendRenew // false
 			}
-			
+
 			domainUserMapperService(DomainUserMapperService)
-			
+
 			userDetailsFromDomainClassFactory(DefaultUserDetailsFromDomainClassFactory)
 
 			/*
@@ -55,24 +43,5 @@ At this time, ignores local role table.
 			}
 		}
 
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+	}
 }
--- src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/cas/CasAuthenticationUserDetailsService.groovy
+++ src/groovy/org/codehaus/groovy/grails/plugins/springsecurity/cas/CasAuthenticationUserDetailsService.groovy
@@ -1,6 +1,5 @@
 package org.codehaus.groovy.grails.plugins.springsecurity.cas
 
-import org.codehaus.groovy.grails.plugins.springsecurity.GrailsUser
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 import org.jasig.cas.client.validation.Assertion
 import org.springframework.security.cas.userdetails.AbstractCasAssertionUserDetailsService
@@ -15,51 +14,51 @@ import org.springframework.security.core.userdetails.UserDetails
  */
 class CasAuthenticationUserDetailsService extends
 	AbstractCasAssertionUserDetailsService {
-	
+
 	/**
 	 * Some Spring Security classes (e.g. RoleHierarchyVoter) expect at least one role, so
 	 * we give a user with no granted roles this one which gets past that restriction but
 	 * doesn't grant anything.
 	 */
 	private static final List NO_ROLES = [new GrantedAuthorityImpl(SpringSecurityUtils.NO_ROLE)]
-	 
+
 	/**
 	 * Should we check for and pull apart Authorities that have been concatenated together? - seems to happen in the cas client lib
 	 */
 	Boolean CONCATENATED_AUTHORITIES_CHECK = true
-	                            		
+
 	private GrantedAuthorityFromAssertionAttributesUserDetailsService grantedAuthoritiesService
-	/** 
+	/**
 	 * Dependency injection for Getting Authorities from CAS
 	 * Provide the attribute name/s that will contain role information from cas
 	 */
-	public void setAuthorityAttribNamesFromCas(authorityAttribNamesFromCas){
+	void setAuthorityAttribNamesFromCas(authorityAttribNamesFromCas){
 		grantedAuthoritiesService = new GrantedAuthorityFromAssertionAttributesUserDetailsService(
-			authorityAttribNamesFromCas.toArray(new String[authorityAttribNamesFromCas.size()]))
+			authorityAttribNamesFromCas as String[])
 	}
-		
+
 	/** Dependency injection for creating and finding Users **/
 	DomainUserMapperService userMapper
-	
+
 	/** Dependency injection for creating userDetails objects **/
 	UserDetailsFromDomainClassFactory userDetailsFromDomainClassFactory
-	
+
 	@Override
 	protected UserDetails loadUserDetails(Assertion casAssert) {
 		//look up user profile in database
 		def user = userMapper.findUserByUsername(casAssert.getPrincipal().getName())
-		
+
 		//Create the user profile if it does not already exist
 		if(!user){
 			user = userMapper.newUser(casAssert.getPrincipal().getName(), casAssert.principal)
 		}
-		
+
 		//authorities
 		def casUser = grantedAuthoritiesService.loadUserDetails(casAssert)
-		def casAuthorities = [] 
-		
+		def casAuthorities = []
+
 		casUser.authorities.each{ authBundle ->
-			
+
 			//check to see if some authorities were concatenated together
 			if(CONCATENATED_AUTHORITIES_CHECK && authBundle.authority.matches(/^\[.*\]$/)){
 				def concattedAuths = authBundle.authority
@@ -70,15 +69,15 @@ class CasAuthenticationUserDetailsService extends
 				concattedAuths.tokenize(",").each{
 					casAuthorities.add(new GrantedAuthorityImpl(it.stripIndent()))
 				}
-				
+
 			}else{
 				casAuthorities.add(authBundle)
 			}
-			
+
 		}
 
 		if (!casAuthorities) casAuthorities=NO_ROLES
-		
+
 		return userDetailsFromDomainClassFactory.createUserDetails(user, casAuthorities)
 	}
 
